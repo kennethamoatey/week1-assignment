6b0b1ad6f6f3d34ded3c20d8dedb0b4e4a6bd66d
kenneth amoatey <kenneth.amoatey@gmail.com>
2024-06-03 22:06:25 +0000

    assigment

--- DOM.js
+++ DOM.js
@@ -1,6 +1,11 @@
 
 
-document.getElementById('heading1').innerHTML="Hello world"
+/*document.getElementById('heading1').innerHTML="Hello world"
 
 let nameChange=document.getElementsByTagName('h1')[0]
-nameChange.innerHTML='Change Text'
+nameChange.innerHTML='Change Text'*/
+
+/*document.body.innerHTML=" Hello world"
+document.write('hello world')*/
+
+document.getElementById('heading1').innerHTML="kenneth amoatey"
