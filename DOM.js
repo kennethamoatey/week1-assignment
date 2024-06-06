afca6f1417b9975216fc417baa349419e0c36ce9
kenneth amoatey <kenneth.amoatey@gmail.com>
2024-06-06 22:40:52 +0000

    addition

--- DOM.js
+++ DOM.js
@@ -6,9 +6,35 @@ let nameChange=document.getElementsByTagName('h1')[0]
 nameChange.innerHTML='Change Text'*/
 
 /*document.body.innerHTML=" Hello world"
-document.write('hello world')*/
+document.write('hello world')
 
 document.getElementById('heading1').innerHTML="kenneth amoatey"
 
 let nameChange =document.getElementsByTagName('h2')[0]
  nameChange.innerHTML='HELLO WORLD'
+
+let className=document.getElementsByClassName('heading4')[0]
+className.innerHTML='Happy world'
+
+let head=document.getElementById('head')
+function clickMe(){
+    head.innerHTML='cute puppy'
+    alert('button clicked')
+}*/
+class Car{
+    constructor(door,steering,engine,seats){
+        this.door=door
+        this.steering=steering
+        this.engine=engine
+        this.seats=seats
+
+    }
+}
+class Truck extends Car{
+    getFeatures(){
+        return this.door+" "+' large bucket'
+    }
+}
+const User1=new Truck('two','one','one','two')
+console.log(User1.getFeatures())
+console.log(User1)
---
+++ ls
@@ -0,0 +1,17 @@
+[33mcommit 417f0482d4fb46820172db570ff25cf24973ddb5[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: kenneth amoatey <kenneth.amoatey@gmail.com>
+Date:   Tue Jun 4 13:01:11 2024 +0000
+
+    assignment
+
+[33mcommit 6b0b1ad6f6f3d34ded3c20d8dedb0b4e4a6bd66d[m
+Author: kenneth amoatey <kenneth.amoatey@gmail.com>
+Date:   Mon Jun 3 22:06:25 2024 +0000
+
+    assigment
+
+[33mcommit fa12049d1e07f7b82e1faeea34d06e332c080a4c[m
+Author: kenneth amoatey <kenneth.amoatey@gmail.com>
+Date:   Tue May 28 09:35:20 2024 +0000
+
+    assignment
